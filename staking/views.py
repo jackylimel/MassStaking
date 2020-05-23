45cd18c506735a504db02a9f8e7af995ef725752
jackylimel <veryfaint@gmail.com>
2020-05-23 23:15:16 +1000

    save stake holders of another day, set receiving_reward to false if existing holder doesn't exist in new holder list

--- staking/views.py
+++ staking/views.py
@@ -18,12 +18,24 @@ def index(request):
 def populate_stake_holders(request):
     r = requests.get('https://explorerapi.masscafe.cn/v1/explorer/transactions/staking/reward/top/?limit=30')
     data_set = r.json()['data']
-    now = datetime.now()
-    timestamp = datetime.timestamp(now)
-    stake_holders = map(lambda data: _data_to_stake_holder(data, timestamp), data_set)
-    for holder in stake_holders:
-        holder.save()
+    timestamp = datetime.timestamp(datetime.now())
+    new_stake_holders = [_data_to_stake_holder(data, timestamp) for data in data_set]
+    new_stake_holder_addresses = [holder.address for holder in new_stake_holders]
+    existing_stake_holders = list(_load_stake_holders())
+    if len(new_stake_holder_addresses) != 0:
+        filtered = [holder for holder in existing_stake_holders if holder.address not in new_stake_holder_addresses]
+        for filtered_holder in filtered:
+            filtered_holder.receiving_reward = False
+            filtered_holder.save()
+    for new_holder in new_stake_holders:
+        print(new_holder)
+        new_holder.save()
     return HttpResponse("success")
+
+
+def _data_to_stake_holder(data, timestamp):
+    return StakeHolder(address=data['address'], total_amount=data['total_amount'],
+                       order=data['id'], timestamp=timestamp, receiving_reward=True)
 
 
 def get_stake_holders(request):
@@ -31,16 +43,14 @@ def get_stake_holders(request):
     transaction_view_models = list(map(lambda tx: TransactionViewModel(tx), _load_transactions()))
 
     holder_view_models = list(map(lambda holder: _map_stake_holder_to_view_model(holder, transaction_view_models),
-                             stake_holders))
+                                  stake_holders))
 
     sorted_transaction_view_models = sorted(transaction_view_models, key=lambda tx: tx.timestamp)
 
     data_frame = pandas.DataFrame.from_records([vars(vm) for vm in sorted_transaction_view_models])
     tx_sum = data_frame.groupby('unlocking_day')['total'].sum()
     dic = tx_sum.to_dict()
-    tx_sum_list = list(map(
-        lambda tx: {'date': tx.to_pydatetime().date,
-                    'value': dic[tx]}, dic))
+    tx_sum_list = list(map(lambda tx: {'date': tx.to_pydatetime().date, 'value': dic[tx]}, dic))
 
     total = reduce(lambda accu, result: accu + float(result.total_amount), stake_holders, 0)
 
@@ -62,7 +72,7 @@ def _map_stake_holder_to_view_model(stakeholder, transaction_view_models):
 
 def _load_stake_holders():
     return filter(lambda holder: holder.address not in Constants.official_addresses,
-                       StakeHolder.objects.filter(receiving_reward=True))
+                  StakeHolder.objects.filter(receiving_reward=True))
 
 
 def _load_transactions():
